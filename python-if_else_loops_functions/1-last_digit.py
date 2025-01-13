c873426f1c582dd667aca9d11cea73ddc1861af8
meurtphy <erwanmpn5@gmail.com>
2025-01-13 17:39:55 +0100

    commentaireerreur

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
     last_digit = -last_digit
 if last_digit > 5:
     print(f"{str} {number} is {last_digit} and is greater than 5")
-elif last_digit < 6 and last_digit != 0:  # Condition explicite
+elif last_digit < 6 and last_digit != 0:
     print(f"{str} {number} is {last_digit} and is less than 6 and not 0")
 else:
     print(f"{str} {number} is {last_digit} and is 0")
