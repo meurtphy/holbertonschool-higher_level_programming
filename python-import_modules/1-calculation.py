4e485c798e0467efeea7412d4cba91ff427a236b
meurtphy <erwanmpn5@gmail.com>
2025-01-15 11:29:34 +0100

    errreurmul

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,5 +7,5 @@ b = 5
 
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, div(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
