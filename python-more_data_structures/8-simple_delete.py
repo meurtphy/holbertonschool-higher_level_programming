10d55df3fd712c72d0c0955adf1dff1ae61e6dce
meurtphy <erwanmpn5@gmail.com>
2025-01-21 17:48:43 +0100

    deldct

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key in a dictionary.
+    - If the key exists, it is removed.
+    - If the key does not exist, the dictionary remains unchanged.
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
