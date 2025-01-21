0761ca32a234d49bfece29fc4aea306a59b421eb
meurtphy <erwanmpn5@gmail.com>
2025-01-21 17:40:37 +0100

    BJPPR

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,4 @@ def only_diff_elements(set_1, set_2):
     """
     Returns a set of all elements present in only one set.
     """
-    # Use the symmetric difference operator to find elements in either set but not both
     return set_1 ^ set_2
