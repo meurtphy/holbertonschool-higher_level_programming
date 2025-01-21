68a2e94600075c9e06f303bc6b9aeacf837f1100
meurtphy <erwanmpn5@gmail.com>
2025-01-21 17:45:53 +0100

    moreinformation

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,7 @@
 def print_sorted_dictionary(a_dictionary):
     """
     Prints a dictionary by ordered keys.
-    Keys are sorted alphabetically, and values are printed in the format key: value.
+    Keys are sorted alphabetically,
     """
     for key in sorted(a_dictionary):
         print(f"{key}: {a_dictionary[key]}")
--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds a key/value pair in a dictionary.
+    - If the key exists, its value is updated.
+    - If the key does not exist, it is added.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
