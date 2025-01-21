ce3d632f92fec114f3a76fc01dd0dfd4ea4eb005
meurtphy <erwanmpn5@gmail.com>
2025-01-21 17:43:45 +0100

    ilt

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys.
+    Keys are sorted alphabetically, and values are printed in the format key: value.
+    """
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")
