bef0ddffb90ca239b40028c2ee046d49224a9726
meurtphy <erwanmpn5@gmail.com>
2025-01-21 17:54:22 +0100

    molly

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """
+    Returns the key with the biggest integer value in the dictionary.
+    If no scores are found or the dictionary is None, returns None.
+    """
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
