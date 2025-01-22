0ac28b54cebae2a871d99b7a82507f420d343518
meurtphy <erwanmpn5@gmail.com>
2025-01-22 13:41:29 +0100

    safe_print_list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             raise
         except Exception:
-              pass
-        print("")
-        return printed
+            pass
+    print("")
+    return printed
