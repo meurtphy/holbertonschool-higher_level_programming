2f151b1be04f155e5f27a741d5bc9a73271e5c88
meurtphy <erwanmpn5@gmail.com>
2025-01-24 10:03:35 +0100

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,26 +3,26 @@
 Module qui contient la fonction add_integer.
 """
 
+
 def add_integer(a, b=98):
     """
-    Ajoute deux entiers.
+    Ajoute deux nombres après les avoir convertis en entiers.
 
     Args:
-        a (int or float): Le premier nombre à ajouter.
-        b (int or float, optional): Le deuxième nombre à ajouter. Par défaut, 98.
+        a (int or float): Le premier nombre.
+        b (int or float, optional): Le deuxième nombre, par défaut 98.
 
     Returns:
-        int: La somme de a et b.
+        int: La somme des deux nombres convertis en entiers.
 
     Raises:
-        TypeError: Si a ou b ne sont pas des entiers ou des flottants.
+        TypeError: Si a ou b ne sont ni des entiers
+        ni des flottants.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
     return a + b
