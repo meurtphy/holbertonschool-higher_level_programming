0b409e92e31d09082a1bfcca21c9cc238ee147f5
meurtphy <erwanmpn5@gmail.com>
2025-01-27 12:48:37 +0100

    typeErrorsquare1

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""2-square.py"""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size = 0):
+        """
+        Initialize the square with a private size attribute.
+        Validate that size is an integer and >= 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
