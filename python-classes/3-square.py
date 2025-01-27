95732b9cf75f37cba39654723136e41b72558151
meurtphy <erwanmpn5@gmail.com>
2025-01-27 14:40:35 +0100

    Area25

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-call
-"""
+"""3-square.py"""
 
 
 class Square:
     """A class that defines a square."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         Initialize the square with a private size attribute.
         Validate that size is an integer and >= 0.
@@ -18,4 +16,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def size
+    def area(self):
+        """Calculate and return the current square area."""
+        return self.__size ** 2
