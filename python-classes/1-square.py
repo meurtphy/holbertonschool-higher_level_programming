d1dfff99ba3e3bb8efa8264cd3c562672c8e4551
meurtphy <erwanmpn5@gmail.com>
2025-01-27 11:02:58 +0100

    secretsquare

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
