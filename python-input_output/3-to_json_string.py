d6dafab3906aeb2bb09291947dc1e310bcd87b52
meurtphy <erwanmpn5@gmail.com>
2025-02-10 13:43:22 +0100

    correctionduchr

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,6 +8,6 @@ import json
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object (string).
+    Returns the JSON representation
     """
     return json.dumps(my_obj)
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Module 4-from_json_string
+Contains a function that returns an object
+represented by a JSON string.
 """
-les commentaire je trouve sa banal pour se type de projet
-"""
-
 
 import json
 
+
 def from_json_string(my_str):
     """
-    Returns an object (Python data structure) represented by a JSON string.
+    Returns an object (Python data structure)
     """
     return json.loads(my_str)
