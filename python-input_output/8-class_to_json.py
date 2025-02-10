5d2cc5ab070dad065ab8d3e799697a7e436c4542
meurtphy <erwanmpn5@gmail.com>
2025-02-10 14:26:33 +0100

    tesla

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Module that returns the dictionary description for JSON serialization of an object.
+Module that returns the dictionary description
 """
+
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple
+    Returns the dictionary description
     """
     return obj.__dict__
