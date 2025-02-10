194de20da898d5589ebb8aeceee08cfea36cf765
meurtphy <erwanmpn5@gmail.com>
2025-02-10 11:11:26 +0100

    dreamof

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    :param filename: Name of the file.
+    :param text: String to write.
+    :return: Number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
