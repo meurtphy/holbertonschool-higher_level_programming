698894ecfccfc04247bcf39fc47bd729b85b3c23
meurtphy <erwanmpn5@gmail.com>
2025-02-10 15:01:02 +0100

    doe

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -14,19 +14,25 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieve dictionary representation of a Student instance.
 
-        If attrs is a list of strings, only attributes with names contained in
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
+        If attrs is a list of strings, only attributes whose names are in
+        this list must be retrieved. Otherwise, all attributes must be
+        retrieved.
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if (isinstance(attrs, list)
+                and all(isinstance(attr, str) for attr in attrs)):
+            return {
+                key: getattr(self, key)
+                for key in attrs
+                if hasattr(self, key)
+            }
         return self.__dict__
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student instance using a dictionary.
 
         Args:
-            json (dict): A dictionary with key/value pairs to update the Student.
+            json (dict): A dictionary with key/value pairs to update the
+                Student.
         """
         for key, value in json.items():
             setattr(self, key, value)
