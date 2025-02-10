5a5e71c4cf41073440182cdff9484d11e6dcb3e5
meurtphy <erwanmpn5@gmail.com>
2025-02-10 14:13:05 +0100

    5-6

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-import sys
-from os import path
+""" save """
 
-# Import the required functions
+import sys
+
+
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Define the filename for storing the list
 filename = "add_item.json"
 
-# Load existing data from the file if it exists, otherwise start with an empty list
 if path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-# Add all command-line arguments to the list
 items.extend(sys.argv[1:])
 
-# Save the updated list back to the file in JSON format
 save_to_json_file(items, filename)
