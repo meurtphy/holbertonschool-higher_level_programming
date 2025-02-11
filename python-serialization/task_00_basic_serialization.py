d2596bced210e4ec1ad208a308d2be5bc83af2cb
meurtphy <erwanmpn5@gmail.com>
2025-02-11 17:55:16 +0100

    test2

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
-"""Module pour sérialiser un dictionnaire Python en JSON et le sauvegarder dans un fichier."""
-
-import json  # Import du module json
+import json
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Sérialise un dictionnaire Python en JSON et l'enregistre dans un fichier.
+    """Serialize dict to JSON and save to file."""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-    :param data: Dictionnaire à sérialiser.
-    :param filename: Nom du fichier dans lequel enregistrer les données.
-    """
-    try:
-        # Ouvrir le fichier en mode écriture ('w') et sérialiser les données
-        with open(filename, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-        print(f"Données sauvegardées dans {filename}")
-
-    except Exception as e:
-        print(f"Erreur lors de la sauvegarde : {e}")
+def load_and_deserialize(filename):
+    """Load JSON from file and return dict."""
+    with open(filename, 'r') as file:
+        return json.load(file)
