6a58177a3648e25961a492e6d98bcf8bb9b3a402
meurtphy <erwanmpn5@gmail.com>
2025-03-03 16:12:36 +0100

    bonne sortie des score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Liste tous les enregistrements de second_table en affichant score et name
+-- Triés par score du plus grand au plus petit
+SELECT score, name FROM second_table ORDER BY score DESC;
