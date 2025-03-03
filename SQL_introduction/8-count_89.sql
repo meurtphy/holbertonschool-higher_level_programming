2807c33c515445bc8b6fe3aaf65f0dcb67a18314
meurtphy <erwanmpn5@gmail.com>
2025-03-03 16:10:34 +0100

    id, name, score

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Compte le nombre de fois où id = 89 dans first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Crée la table second_table si elle n'existe pas
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insère les valeurs demandées
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
