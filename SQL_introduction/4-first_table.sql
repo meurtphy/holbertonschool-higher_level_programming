f0a27393fa6f647670bf9401a109700c21e52114
meurtphy <erwanmpn5@gmail.com>
2025-03-03 15:54:51 +0100

    probleme en local meme probleme sur la review ?

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Crée la table first_table si elle n'existe pas
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Affiche la structure complète de la table first_table
+SHOW CREATE TABLE first_table;
