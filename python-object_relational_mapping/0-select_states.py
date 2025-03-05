463a8a897f9ac8849c08d2eac7a7ee60731aae7c
meurtphy <erwanmpn5@gmail.com>
2025-03-05 14:06:24 +0100

    probleme de connexion

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
-"""
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # 1. Récupération des arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # 2. Connexion à la base de données
+    # Connexion à la base de données
     db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
+        host="127.0.0.1",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306
     )
 
-    # 3. Création d’un curseur pour exécuter les requêtes
+    # Création du curseur pour exécuter des requêtes
     cur = db.cursor()
 
-    # 4. Exécution de la requête
-    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    # Exécuter la requête SQL
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # 5. Récupération et affichage des résultats
-    rows = cur.fetchall()
-    for row in rows:
+    # Récupérer et afficher les résultats
+    for row in cur.fetchall():
         print(row)
 
-    # 6. Fermeture du curseur et de la connexion
+    # Fermer le curseur et la connexion
     cur.close()
     db.close()
