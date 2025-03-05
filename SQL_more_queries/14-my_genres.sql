67fba9c8f9d9b95be19638d83e5d8c6c426378f8
meurtphy <erwanmpn5@gmail.com>
2025-03-05 10:45:32 +0100

    Ajout du script SQL pour afficher les genres de la série Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Sélectionner tous les genres associés à la série "Dexter"
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
