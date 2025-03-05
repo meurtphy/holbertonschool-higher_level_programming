5afcbe9765149ebb96b075f453430ef67a5deff6
meurtphy <erwanmpn5@gmail.com>
2025-03-05 13:09:54 +0100

    topujour un problerme

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+"""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Récupération des arguments
+    # 1. Récupération des arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connexion à la base de données
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    # 2. Connexion à la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
 
+    # 3. Création d’un curseur pour exécuter les requêtes
+    cur = db.cursor()
 
-    # Création d'un curseur pour exécuter la requête
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # 4. Exécution de la requête
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
 
-    # Récupération et affichage des résultats
-    for row in cursor.fetchall():
+    # 5. Récupération et affichage des résultats
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    # Fermeture du curseur et de la connexion
-    cursor.close()
+    # 6. Fermeture du curseur et de la connexion
+    cur.close()
     db.close()
