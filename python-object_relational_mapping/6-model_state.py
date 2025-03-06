59ac42a231ed74ad5131cd17302e387231ec10dc
meurtphy <erwanmpn5@gmail.com>
2025-03-06 11:10:25 +0100

    probleme j avais plus sql achlmy

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a State and an instance of Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+# Création d'une instance de Base
+Base = declarative_base()
+
+
+class State(Base):
+    """Représente la table `states` en SQLAlchemy"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
