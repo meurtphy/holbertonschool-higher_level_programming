749e079d3ca0bf30a620770fe18268f462d58c20
meurtphy <erwanmpn5@gmail.com>
2025-03-06 12:27:42 +0100

    commentaire trop long

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State and an instance of Base = declarative_base()
+Contains the class definition of a State
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
