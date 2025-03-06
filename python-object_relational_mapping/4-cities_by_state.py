5fb937b10f3c7bf86b36ab8cf66430e4ec6dcd26
meurtphy <erwanmpn5@gmail.com>
2025-03-06 10:56:58 +0100

    multiple porblme de pycodestyle sur la 3 et la 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,48 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities from the database hbtn_0e_4_usa
-"""
-
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
 
-def list_cities():
-    """Function that lists all cities with their state names"""
-    if len(sys.argv) != 4:
-        return
-
+if __name__ == "__main__":
     # Connexion à la base de données
     db = MySQLdb.connect(
-    host="172.18.0.2",  # Mets l'IP récupérée ici
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    port=3306
- )
-
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
     # Création du curseur
     cur = db.cursor()
 
-    # Requête SQL pour lister toutes les villes avec leur état
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """
-
+    # Exécution de la requête SQL
+    query = """SELECT cities.id, cities.name, states.name
+               FROM cities JOIN states
+               ON cities.state_id = states.id
+               ORDER BY cities.id ASC"""
     cur.execute(query)
 
-    # Afficher les résultats
-    for row in cur.fetchall():
+    # Affichage des résultats
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
     # Fermeture du curseur et de la connexion
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    list_cities()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" Script that lists all cities of a given state from the database hbtn_0e_4_usa """
+""" Script that lists all cities of a state using MySQLdb """
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Connexion à la base de données
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        db=sys.argv[3]
     )
 
     # Création du curseur
     cur = db.cursor()
 
-    # Requête SQL sécurisée avec un paramètre
-    cur.execute("SELECT cities.name FROM cities \
-                 JOIN states ON cities.state_id = states.id \
-                 WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+    # Requête SQL sécurisée pour éviter les injections
+    query = "SELECT cities.name FROM cities \
+             JOIN states ON cities.state_id = states.id \
+             WHERE states.name = %s ORDER BY cities.id ASC"
 
-    # Récupération des résultats
-    cities = cur.fetchall()
-    print(", ".join(city[0] for city in cities))
+    cur.execute(query, (sys.argv[4],))
 
-    # Fermeture de la connexion
+    # Récupérer les résultats et les afficher
+    rows = cur.fetchall()
+    print(", ".join(row[0] for row in rows))
+
+    # Fermer les connexions
     cur.close()
     db.close()
