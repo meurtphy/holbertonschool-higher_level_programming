a7eae1457cb463e4ba6b0c4b8824b52746c5f51e
meurtphy <erwanmpn5@gmail.com>
2025-03-07 13:43:16 +0100

    bonne sortie 2

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain 'a' from the database"""
+"""Lists all State objects that contain the letter 'a' from the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Connexion MySQL (forcer 127.0.0.1 pour éviter le problème de socket)
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
+    # Création du moteur avec 127.0.0.1 au lieu de localhost
+    engine = create_engine("mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
 
-    # Création de session
+    # Création de la session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Récupération des États contenant 'a'
+    # Récupération des états contenant la lettre 'a' triés par id croissant
     states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
     # Affichage des résultats
