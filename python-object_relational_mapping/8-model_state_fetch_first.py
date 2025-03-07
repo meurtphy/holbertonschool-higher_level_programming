316b59058445756d73e9bdfb7e9f57fc608f35a6
meurtphy <erwanmpn5@gmail.com>
2025-03-07 12:32:52 +0100

    bonne sortie mais pas la bonne review

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Création de l'engine avec localhost et port 3306
+    # Utilise 127.0.0.1 au lieu de localhost
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
