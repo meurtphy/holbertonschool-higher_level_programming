1b49d958daf19768fde0eeb0ba5708ca2a06b4f4
meurtphy <erwanmpn5@gmail.com>
2025-03-07 12:31:12 +0100

    ?

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Création de l'engine avec l'IP 127.0.0.1 au lieu de localhost
+    # Création de l'engine avec localhost et port 3306
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # Récupération du premier état
     first_state = session.query(State).order_by(State.id).first()
 
-    # Affichage du résultat exactement comme demandé
+    # Affichage du résultat EXACTEMENT comme demandé
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
