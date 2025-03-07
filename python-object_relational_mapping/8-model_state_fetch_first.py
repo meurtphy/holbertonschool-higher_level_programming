155385ab1fd2900760ec8c0f8925b999ff579f12
meurtphy <erwanmpn5@gmail.com>
2025-03-07 12:24:07 +0100

    Correction de la connexion MySQL pour utiliser 127.0.0.1

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""
-Prints the first State object from the database hbtn_0e_6_usa.
-"""
+"""Prints first State object from database"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-    "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]
-    ),
-    pool_pre_ping=True
-)
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create session factory
     Session = sessionmaker(bind=engine)
+
+    # Create session
     session = Session()
 
+    # Query first State object
     first_state = session.query(State).order_by(State.id).first()
 
+    # Print result or Nothing if empty
     if first_state:
-        print(f"{first_state.id}: {first_state.name}")
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
 
+    # Close session
     session.close()
