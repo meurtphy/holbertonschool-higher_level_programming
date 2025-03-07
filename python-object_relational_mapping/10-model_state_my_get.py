5c13800aba950f00274a7b4a7fea804ea83c0cc4
meurtphy <erwanmpn5@gmail.com>
2025-03-07 14:47:10 +0100

    un probleme incoryable

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -7,23 +7,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Connexion MySQL avec 127.0.0.1 (comme pour la tâche 8)
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        )
     )
 
-    # Création de la session SQLAlchemy
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Recherche sécurisée d'un état avec le nom donné
+    # Query state by name
     state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-    # Affichage conforme aux attentes
-    print(state.id if state else "Not found")
+    # Print state id if found, otherwise print Not found
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
 
-    # Fermeture propre de la session
     session.close()
