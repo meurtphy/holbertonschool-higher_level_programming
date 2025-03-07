f955982d8e2c99372bcb75bc52e96e1b7b666a35
meurtphy <erwanmpn5@gmail.com>
2025-03-07 11:30:49 +0100

    Correction des scripts SQLAlchemy pour les tâches 8 à 11

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as an argument from the database.
+Prints the State object with the name passed as argument from the database hbtn_0e_6_usa.
 """
 import sys
 from sqlalchemy import create_engine
@@ -8,27 +8,22 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Connexion à MySQL en localhost:3306
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
 
-    # Création de la session SQLAlchemy
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Récupération de l'état avec le nom spécifié (évite SQL Injection)
     state_name = sys.argv[4]
     state = session.query(State).filter(State.name == state_name).first()
 
-    # Affichage du résultat
     if state:
         print(state.id)
     else:
         print("Not found")
 
-    # Fermeture de la session
     session.close()
--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa.
+Adds the State object “Louisiana” to the database hbtn_0e_6_usa.
 """
 import sys
 from sqlalchemy import create_engine
@@ -18,9 +18,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
 
-    for state in states_with_a:
-        print(f"{state.id}: {state.name}")
+    print(new_state.id)
 
     session.close()
