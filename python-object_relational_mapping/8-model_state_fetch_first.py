48b75cda3114cd6f0a0b7e5c8bd835d03f5f56df
meurtphy <erwanmpn5@gmail.com>
2025-03-07 12:25:48 +0100

    Correction format de sortie et connexion MySQL

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,24 +6,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Création de l'engine avec l'IP 127.0.0.1 au lieu de localhost
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
 
-    # Create session factory
+    # Création de la session
     Session = sessionmaker(bind=engine)
-
-    # Create session
     session = Session()
 
-    # Query first State object
+    # Récupération du premier état
     first_state = session.query(State).order_by(State.id).first()
 
-    # Print result or Nothing if empty
+    # Affichage du résultat EXACTEMENT comme demandé
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
 
-    # Close session
+    # Fermeture de la session
     session.close()
