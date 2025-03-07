489e042cf2ead04adc581ab84e40815f0cd9a570
meurtphy <erwanmpn5@gmail.com>
2025-03-07 13:51:17 +0100

    10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""Finds a State object with the given name"""
+"""Prints the State object with the given name from the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Connexion MySQL (forcer 127.0.0.1)
+    # Création de l'engine en forçant l'IP 127.0.0.1 pour éviter les erreurs
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+        "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
 
-    # Création de session
+    # Création de la session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Recherche de l'État
+    # Recherche du premier état correspondant EXACTEMENT au nom donné
     state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-    # Affichage du résultat
+    # Affichage strictement identique à l'exemple attendu
     if state:
         print(state.id)
     else:
         print("Not found")
 
-    # Fermeture de la session
+    # Fermeture propre de la session
     session.close()
