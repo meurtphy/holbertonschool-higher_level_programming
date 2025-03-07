355aae15f93a27e2b5a692baa47758c1a9ec1d1e
meurtphy <erwanmpn5@gmail.com>
2025-03-07 13:48:23 +0100

    pycodestle 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,8 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     # Connexion à MySQL avec 127.0.0.1 pour éviter les erreurs de socket
-    engine = create_engine("mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
     )
 
     # Création de la session SQLAlchemy
@@ -16,7 +19,12 @@ if __name__ == "__main__":
     session = Session()
 
     # Requête : récupérer les États contenant 'a', triés par ID croissant
-    states = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+    states = (
+        session.query(State)
+        .filter(State.name.ilike('%a%'))
+        .order_by(State.id)
+        .all()
+    )
 
     # Affichage strictement identique à l'exemple
     for state in states:
