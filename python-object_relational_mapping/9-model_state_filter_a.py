462b3fe98af2479363be20faf42f0da3ff837e80
meurtphy <erwanmpn5@gmail.com>
2025-03-07 13:22:37 +0100

    le probleme etais Forcer SQLAlchemy à utiliser TCP/IP en remplaçant localhost par 127.0.0.1

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""
-Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa.
-"""
+"""Lists all State objects that contain the letter 'a'"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
+    # Connexion MySQL avec 127.0.0.1 pour éviter les erreurs de socket
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
+        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
 
+    # Création de la session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+    # Récupération des États contenant 'a' en ordre croissant d'ID
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
+    # Affichage des résultats
     for state in states_with_a:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
 
+    # Fermeture de la session
     session.close()
